bfd90c9a5516ecb323e3e93558fd204019fdbf49
samarth-nagar <samarthxma2009@gmail.com>
2024-03-10 23:36:01 +0530

    added tailwind

--- .weterm.lua
+++ .weterm.lua
@@ -2,21 +2,22 @@ local wezterm = require 'wezterm';
 
 local config = {
     color_scheme = 'One Dark (Gogh)',
-    window_background_opacity = 0.75,
+    window_background_opacity = 0.90,
     window_decorations = "NONE",
     hide_tab_bar_if_only_one_tab = true,
     font_size = 16,
-    window_padding = { left = 6, right = 0, top = 3, bottom = 1 },
+    window_padding = { left = 7, right = 0, top = 4, bottom = 0 },
     force_reverse_video_cursor = true,
     foreground_text_hsb = {
         hue = 1.0,
-        saturation = 1.05,
-        brightness = 1.05,
+        saturation = 1.10,
+        brightness = 1.2,
     },
     audible_bell = "Disabled"
 };
 
 config.colors = { foreground = "#abb2bf" }
 config.font = wezterm.font('JetBrains Mono', { weight = 'Regular', italic = false })
+
 
 return config;
